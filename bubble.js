1b6a6b66f6892d9f2a4889395a73dbf44230d1f3
unknown <marko.vitkovic@outlook.com>
2021-09-10 13:45:15 +0200

    add regular bbl sort function

--- bubble.js
+++ bubble.js
@@ -1,3 +1,29 @@
+//implementacija bubble sorta
+
+//kreiranje funkcije bblSort
+function bblSort(arr){
+
+    for(var i = 0; i < arr.length; i++){
+
+        //posljednji i element je vec na mjestu
+        for(var j = 0; j < (arr.length -i -1); j++){
+            
+            //provjera dali je element u prethodnoj iteraciji veci nego u sljedecoj
+            if(arr[j] > arr[j + 1]){
+
+                //ako je to tocno, zamjeni elemente
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    //printanje sortiranog niza
+    console.log("Sortirani niz: ",arr);
+}
+
+
 //implementacija optimiziranog bubble sorta
 
 //kreiranje funkcije bubbleSort
@@ -25,9 +51,13 @@ function bubbleSort(arr){
     }
 
     //ispisi array
-    console.log(arr);
+    console.log("Sortirani niz(optimizirani): ",arr);
 }
 
+//nesortirani niz
 var arr = [243, 45, 23, 356, 3, 5346, 35, 5];
 
-bubbleSort(arr)
+
+//prosljedivanje niza funkcijama
+bblSort(arr);
+bubbleSort(arr);
